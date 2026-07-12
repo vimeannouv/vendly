e4d4ce3cb6651d0fb1456fe7a11bdb0230e6fd91
vimeannouv <vimeanouv@gmail.com>
2026-07-12 10:00:33 +0000

    did work

--- src/components/elements/PopupLayer.tsx
+++ src/components/elements/PopupLayer.tsx
@@ -1,22 +1,47 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Box } from "@chakra-ui/react";
 import type { ComponentProps, ReactNode } from "react";
 
 interface PopupLayerProp extends ComponentProps<typeof Flex> {
   children?: ReactNode;
 }
 
-const PopupLayer = ({ }: PopupLayerProp) => {
+const PopupLayer = ({ ...rest }: PopupLayerProp) => {
   return (
+
     <Flex
-      w={"40%"}
-      h={"50%"}
-      bgColor={"blue"}
-      position={"absolute"}
-      zIndex={"2"}
-      top={"50%"}
-      bottom={"50%"}
-      transform={"translate(-50%, -50%)"}
-    ></Flex>
+      w={"100%"}
+      h={"100%"}
+      bgColor={"rgba(0, 0, 0, 0.3)"}
+      position={"fixed"}
+      zIndex={"999"}
+      {...rest}
+    >
+      <Flex
+        w={"70%"}
+        h={"70%"}
+        bgColor={"rgb(255, 255, 255)"}
+        position={"fixed"}
+        zIndex={"2"}
+        top={"50%"}
+        left={"50%"}
+        transform={"translate(-50%, -50%)"}
+        borderRadius={"40px"}
+        flexDir={"row"}
+
+        {...rest}
+      >
+        {/* IMAGE DISPLAY */}
+        <Box flex={1} h={"100%"} bgColor={"red"}>
+
+        </Box>
+        {/* ITEM NAME, PRICE, QUANTITY */}
+        <Flex flex={1} h={"100%"} bgColor={"blue"}>
+
+        </Flex>
+
+      </Flex>
+    </Flex>
+
   );
 };
 
